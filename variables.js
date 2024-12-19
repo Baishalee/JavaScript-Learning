e446acc030326c96c8ebe7d85d58518bcd20279a
Baishalee <baishaleenegi@gmail.com>
2024-12-19 17:28:08 +0530

    add new js file

---
+++ variables.js
@@ -0,0 +1,53 @@
+// Declaring variables without using let, const, and vars
+console.log("Hello World!");
+console.log("Welcome to JavaScript Tutorial");
+fullName = "BAISHALEE NEGI";
+console.log(fullName);
+myAge = 30;
+console.log(myAge);
+
+// Variables are of 3 types
+let variableName = "value";  // Block-scoped variable, cannot be reassigned
+const constantName = 42;    // Block-scoped constant, cannot be reassigned
+var variable_name = true;   // Global-scoped variable which is used in older syntax before 2015, can be reassigned
+
+// Example: Declaring variables with let, const, and var
+let age = 25;                 
+console.log("Age:", age);
+const pi = 3.14;
+console.log("Pi:", pi);
+var city = "New York";       
+console.log("City:", city);
+
+// Data Types
+let stringExample = "Hello, World!";  // String
+let numberExample = 123;             // Number
+let booleanExample = true;           // Boolean
+let nullExample = null;              // Null
+let undefinedExample;                // Undefined
+let objectExample = { key: "value" }; // Object
+let arrayExample = [1, 2, 3];        // Array
+
+// Example: Declaring data types
+const student = {
+    firstName: "John",
+    age: 30,
+    education: "MBA",
+    subjects: ["Management", "Finance", "Marketing", "Project"],
+    isPass: "yes",
+    cgpa:9.0,
+};
+console.log(student);
+console.log(typeof student);
+console.log(student.firstName);
+console.log(typeof student.firstName);
+console.log(student.age);
+console.log(typeof student.age);
+console.log(student.education);
+console.log(typeof student.education);
+console.log(student.subjects);
+console.log(typeof student.subjects);
+console.log(student.isPass);
+console.log(typeof student.isPass);
+console.log(student.cgpa);
+console.log(typeof student.cgpa);
